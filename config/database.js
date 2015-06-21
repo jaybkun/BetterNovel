a28a55b88b813148985e0469b63e9c729b83e4b5
Jay Barra <jay.f.barra@gmail.com>
2015-06-21 09:31:50 -0400

    made the mongodb connection aware of openshift or localhost

--- config/database.js
+++ config/database.js
@@ -1,6 +1,17 @@
 // config/database.js
+
+var connectionString = '127.0.0.1:27017/betternovel';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":"+
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "/" +
+    process.env.OPENSHIFT_APP_NAME;
+}
+
 module.exports = {
 
-    url: "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    url: "mongodb://" + connectionString
 
 };
