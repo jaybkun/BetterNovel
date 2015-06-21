5e49c1f11431938a93bb6322a88b2d423b191d50
Jay Barra <jay.f.barra@gmail.com>
2015-06-21 08:33:39 -0400

    getting ready for openshift deployment

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    url: "mongodb://localhost/betternovel" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    url: "mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/" // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
